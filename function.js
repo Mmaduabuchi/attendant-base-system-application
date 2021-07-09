d1987c92e94f113fb7c6fcb2aa6d4d47f418b350
Emmanuel <emmanuelukwe1@gmail.com>
2021-07-08 17:09:23 -0700

    working on admin page

--- function.js
+++ function.js
@@ -6,3 +6,16 @@ setInterval(() => {
     var time = new Date();
     document.getElementById("time-holder").innerHTML = time.toLocaleTimeString();
 }, 1000);
+
+const menuDivHolder = document.querySelector(".menu-drop-side-holder");
+const menuDiv = document.querySelector(".menu-wrapper");
+
+menuDivHolder.onclick = function () {
+    let displayDivMenu = menuDiv.style.display;
+
+    if(displayDivMenu == "none"){
+        menuDiv.style.display = "block";
+    }else{
+        menuDiv.style.display = "none";
+    }
+}
