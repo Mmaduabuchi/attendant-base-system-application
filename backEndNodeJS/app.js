1d3a8352b443b02f48f8da30c1fe8712ae7b0326
Emmanuel <emmanuelukwe1@gmail.com>
2021-07-20 13:33:12 -0700

    small change on backend app.js

--- backEndNodeJS/app.js
+++ backEndNodeJS/app.js
@@ -33,11 +33,6 @@ app.post('/submit', (req, res)=>{
 
 });
 
-app.get('/test', (req, res) => {
-    res.status(200).send({Data:"Hello"});
-    console.log('Requested')
-})
-
 //listen to the network port
 app.listen(3000, () =>{
 
